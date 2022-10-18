c416104e2571bf19fd13ba6697c1bf11778ca200
aru99 <mohammadarmanmavric@gmail.com>
2022-10-18 15:13:08 +0530

    added sticky navigation

--- omnifood_project/js/script.js
+++ omnifood_project/js/script.js
@@ -33,6 +33,29 @@ allLinks.forEach(function (link) {
     }
   });
 });
+//sticky navigation
+
+const sectionHeroEl = document.querySelector(".section_hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      console.log(ent);
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      console.log(ent);
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
